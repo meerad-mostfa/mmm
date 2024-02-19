6f5d105024bc1541edd0624d4eb7b2ce7cc0a555
meerad-mostfa <146655982+meerad-mostfa@users.noreply.github.com>
2024-02-19 19:57:35 +0200

    i

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let name="meerad abdo";
+  let age=19;
+
+  return (
+  <>
+    <h2>heelo {name}</h2>
+    <p>You age is {age}</p>
+  </>
+  )
+}
+
+export default App
